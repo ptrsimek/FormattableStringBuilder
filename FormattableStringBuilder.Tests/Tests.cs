291dc516372a2b1dd18483e63917bf5a9fdb82cb
Petr Šimek <petr@ptrsimek.com>
2019-09-02 19:26:40 +0200

    Added unit tests

---
+++ FormattableStringBuilder.Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace FormattableStringBuilder.Tests
+{
+    using System;
+    using Xunit;
+    
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var b = new FormattableStringBuilder();
+            b.Append($"x = {42}");
+
+            var fs = b.ToFormattableString();
+
+            Assert.Equal(fs.Format, "x = {0}");
+            Assert.Equal(fs.ArgumentCount, 1);
+            Assert.Equal(fs.GetArgument(0), 42);
+        }
+    }
+}
